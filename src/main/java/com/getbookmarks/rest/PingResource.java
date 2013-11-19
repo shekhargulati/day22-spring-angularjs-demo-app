b161d3d8f1227ffa86bbddeb1aaa114d33f7cb3b
Shekhar Gulati <shekhargulati84@gmail.com>
2013-11-19 19:30:44 +0530

    wrong REST url

--- src/main/java/com/getbookmarks/rest/PingResource.java
+++ src/main/java/com/getbookmarks/rest/PingResource.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RequestMapping("/api/v1/ping")
+@RequestMapping("/ping")
 @Controller
 public class PingResource {
 
